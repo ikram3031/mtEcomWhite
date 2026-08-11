fbaf22548314d3b98650cde8b047b4da05194b37
ikram3031 <mdikram.3031@gmail.com>
2026-08-11 09:53:43 +0600

    [AB-67] Split Monorepo & Remove Storefront Frontend

--- frontend/src/core/utils/formatCurrency.js
+++
@@ -1,7 +0,0 @@
-export function formatBDT(value) {
-  const n = Number(value) || 0;
-  // simple formatting with Bengali Taka symbol
-  return `৳ ${n.toLocaleString('en-US')}`;
-}
-
-export default formatBDT;
