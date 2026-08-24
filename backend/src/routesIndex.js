37d6bf6adb3675fed6d74e29ea4c48d9fe094ba4
ikram3031 <mdikram.3031@gmail.com>
2026-08-24 23:05:20 +0600

    AB85(feat): implement dashboard products POST aggregation search api

--- backend/src/routesIndex.js
+++ backend/src/routesIndex.js
@@ -22,11 +22,13 @@ import subscriberRouter from "./routes/SubscriberRoute.js";
 import contactRouter from "./routes/ContactRoute.js";
 import storeUtilsRouter from "./routes/StoreUtilsRoute.js";
 import reportsRouter from "./routes/ReportsRoute.js";
+import dashProductRouter from "./dashboard/routes/dashProduct.route.js";
 import express from "express";
 import { searchProducts } from "./controllers/SearchController.js";
 
 const coreRouter = Router();
 
+coreRouter.use("/dash/products", dashProductRouter);
 coreRouter.use("/products", productsRouter);
 coreRouter.use("/reviews", reviewRouter);
 coreRouter.use("/images", imagesRouter);
